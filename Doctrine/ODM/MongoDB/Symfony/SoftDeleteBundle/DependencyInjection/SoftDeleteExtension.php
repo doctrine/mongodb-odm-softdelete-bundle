a08e0f0e6cf1852631ec96e7d40a8bd74a7d1723
Jeremy Mikola <jmikola@gmail.com>
2011-02-11 14:06:24 -0500

    Updated SoftDeleteExtension to use FileLocator per recent DIC changes

--- Doctrine/ODM/MongoDB/Symfony/SoftDeleteBundle/DependencyInjection/SoftDeleteExtension.php
+++ Doctrine/ODM/MongoDB/Symfony/SoftDeleteBundle/DependencyInjection/SoftDeleteExtension.php
@@ -3,6 +3,7 @@
 namespace Doctrine\ODM\MongoDB\Symfony\SoftDeleteBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -11,8 +12,8 @@ class SoftDeleteExtension extends Extension
     public function configLoad(array $configs, ContainerBuilder $container)
     {
         if (!$container->hasDefinition('soft_delete.manager')) {
-            $loader = new XmlFileLoader($container);
-            $loader->load(__DIR__ . '/../Resources/config/soft_delete.xml');
+            $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+            $loader->load('soft_delete.xml');
         }
     }
 
